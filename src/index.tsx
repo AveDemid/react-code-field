0745a0b6cf0dd152a392693330d98acf159c8e3a
AveDemid <ave.demid@gmail.com>
2019-10-07 00:48:48 +0300

    Add externals in webpack configuration, installed prettier, add new features in react-code-field

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
 
 const BACKSPACE_KEY = 8;
+const TAB_KEY = 9;
+const LEFT_ARROW = 37;
+const RIGHT_ARROW = 39;
+const E_KEY = 69;
 
 interface ReactCodeFieldProps {
   fields: number;
+  onChange(s: string): void;
+  inputType: "text" | "password" | "number";
+  listBannedChars: string[];
 }
 
 // prettier-ignore
@@ -38,24 +45,38 @@ export const changeValueInArr = (
   return Object.assign([...arr], { [idx]: newValue });
 };
 
-const ReactCodeField = ({ fields }: ReactCodeFieldProps) => {
-  // Input references
+export const ReactCodeField = ({
+  fields,
+  onChange,
+  inputType,
+  listBannedChars
+}: ReactCodeFieldProps) => {
   const fieldRefs = useRef<HTMLInputElement[]>([]);
 
-  //  Array of values
   const [fieldValues, setFieldValues] = useState<string[]>(
     Array(fields).fill("")
   );
 
-  // Focused input
   const [focusedFieldIdx, setFocusedFieldIdx] = useState<number>(0);
 
-  // Handle Change Value
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
 
     const idx = Number(event.target.dataset.idx);
-    const handledValue = event.target.value.split("");
+    let handledValue = event.target.value.split("");
+
+    if (inputType === "number") {
+      handledValue = handledValue.filter(element => {
+        return typeof +element === "number" && isFinite(+element);
+      });
+    }
+
+    if (listBannedChars) {
+      handledValue = handledValue.filter(element => {
+        return !listBannedChars.includes(element);
+      });
+    }
+
     const nextFieldValues = mergeArrays(fieldValues, handledValue, idx);
     const nextFocusedFieldIdx = getNextFocusedFieldIndex(
       idx,
@@ -67,7 +88,6 @@ const ReactCodeField = ({ fields }: ReactCodeFieldProps) => {
     setFocusedFieldIdx(nextFocusedFieldIdx);
   };
 
-  // Handle backspace keydown
   const handleBackspaceKeyDown = (value: string, idx: number) => {
     const nextValue = changeValueInArr(fieldValues, "", idx);
     const nextFocusedFieldIdx = getNextFocusedFieldIndex(idx, -1, fields - 1);
@@ -75,7 +95,30 @@ const ReactCodeField = ({ fields }: ReactCodeFieldProps) => {
     value ? setFieldValues(nextValue) : setFocusedFieldIdx(nextFocusedFieldIdx);
   };
 
-  // Handle keydown
+  const handleLeftKeyDown = (idx: number) => {
+    const nextFocusedFieldIdx = getNextFocusedFieldIndex(idx, -1, fields - 1);
+
+    setFocusedFieldIdx(nextFocusedFieldIdx);
+  };
+
+  const handleRightKeyDown = (idx: number) => {
+    const nextFocusedFieldIdx = getNextFocusedFieldIndex(idx, 1, fields - 1);
+
+    setFocusedFieldIdx(nextFocusedFieldIdx);
+  };
+
+  const handleTabKeyDown = (idx: number, shiftKey: boolean) => {
+    let nextFocusedFieldIdx;
+
+    if (shiftKey) {
+      nextFocusedFieldIdx = getNextFocusedFieldIndex(idx, -1, fields - 1);
+    } else {
+      nextFocusedFieldIdx = getNextFocusedFieldIndex(idx, 1, fields - 1);
+    }
+
+    setFocusedFieldIdx(nextFocusedFieldIdx);
+  };
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     const keyCode = event.keyCode;
     const value = event.currentTarget.value;
@@ -86,31 +129,85 @@ const ReactCodeField = ({ fields }: ReactCodeFieldProps) => {
         event.preventDefault();
         handleBackspaceKeyDown(value, idx);
         break;
+      case LEFT_ARROW:
+        event.preventDefault();
+        handleLeftKeyDown(idx);
+        break;
+      case RIGHT_ARROW:
+        event.preventDefault();
+        handleRightKeyDown(idx);
+        break;
+      case TAB_KEY:
+        event.preventDefault();
+        handleTabKeyDown(idx, event.shiftKey);
+        break;
+      case E_KEY:
+        if (inputType === "number") {
+          event.preventDefault();
+        }
     }
   };
 
-  // Focus Control
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const idx = Number(event.target.dataset.idx);
+
+    if (idx === focusedFieldIdx) {
+      event.target.select();
+    } else {
+      setFocusedFieldIdx(idx);
+    }
+  };
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    const idx = Number(event.currentTarget.dataset.idx);
+
+    if (idx === focusedFieldIdx) {
+      event.currentTarget.select();
+    } else {
+      setFocusedFieldIdx(idx);
+    }
+  };
+
   useEffect(() => {
-    fieldRefs.current[focusedFieldIdx].focus();
+    if (fieldRefs.current[focusedFieldIdx]) {
+      fieldRefs.current[focusedFieldIdx].focus();
+      fieldRefs.current[focusedFieldIdx].select();
+    }
   }, [focusedFieldIdx]);
 
+  useEffect(() => {
+    if (onChange) {
+      onChange(fieldValues.join(""));
+    }
+  }, [fieldValues, onChange]);
+
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       {fieldValues.map((value, idx) => (
         <input
-          type="text"
           key={idx}
           data-idx={idx}
+          type={inputType}
           value={value}
-          onKeyDown={handleKeyDown}
           ref={node => {
-            fieldRefs && node && fieldRefs.current.push(node);
+            node && fieldRefs && fieldRefs.current.push(node);
           }}
-          onChange={handleChange}
+          onInput={handleInput}
+          onFocus={handleFocus}
+          onKeyDown={handleKeyDown}
+          onMouseDown={handleMouseDown}
+          onChange={e => e.preventDefault()}
+          style={{
+            width: "30px",
+            height: "30px",
+            textAlign: "center",
+            fontSize: "20px"
+          }}
         />
       ))}
     </div>
   );
 };
-
-export default ReactCodeField;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.tsx',
+  mode: "production",
+  entry: "./src/index.tsx",
   output: {
-    path: path.resolve('lib'),
-    libraryTarget: 'commonjs2',
+    path: path.resolve("lib"),
+    libraryTarget: "commonjs2",
     filename: "index.js"
   },
   module: {
@@ -18,9 +18,25 @@ module.exports = {
           },
           {
             loader: "ts-loader"
-          },
+          }
         ]
       }
     ]
+  },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+      umd: "react-dom"
+    }
   }
 };
